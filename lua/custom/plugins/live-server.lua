1f84f45058ce45fe6d0e17c8cdf1ceafc934e7ee
Enzo Lazzarini <enzolazzamor@gmail.com>
2024-12-31 13:33:45 -0300

    added plugins

--- lua/custom/plugins/live-server.lua
+++ lua/custom/plugins/live-server.lua
@@ -3,7 +3,11 @@ return {
     'barrett-ruth/live-server.nvim',
     build = 'pnpm add -g live-server',
     cmd = { 'LiveServerStart', 'LiveServerStop' },
-    config = true,
+    config = function()
+      require('live-server').setup {
+        args = { '--port=7000', '--browser=zen-browser' },
+      }
+    end,
     opts = {},
   },
 }
--- lua/custom/plugins/utils.lua
+++ lua/custom/plugins/utils.lua
@@ -14,4 +14,22 @@ return {
     priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
     config = true,
   },
+  {
+    'meatballs/notebook.nvim',
+    config = function()
+      require('notebook').setup {
+        -- Whether to insert a blank line at the top of the notebook
+        insert_blank_line = true,
+
+        -- Whether to display the index number of a cell
+        show_index = true,
+
+        -- Whether to display the type of a cell
+        show_cell_type = true,
+
+        -- Style for the virtual text at the top of a cell
+        virtual_text_style = { fg = 'lightblue', italic = true },
+      }
+    end,
+  },
 }
